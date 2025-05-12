1c7252f4d1ae60c5b1343d4479b9529cad7614d8
N1kolozz <mr.osievi5@gmail.com>
2025-05-12 20:55:43 +0400

    Add Webpack configuration and update package.json dependencies

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  target: 'node',
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
